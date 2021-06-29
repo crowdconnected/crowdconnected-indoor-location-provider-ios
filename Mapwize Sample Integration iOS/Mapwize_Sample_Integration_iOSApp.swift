72a0a49046fb4b9d219c2b324f3de864112df6e0
Robert Tanase <robert.tanase97@gmail.com>
2021-06-29 18:09:46 +0100

    Initial Commit

---
+++ Mapwize Sample Integration iOS/Mapwize_Sample_Integration_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mapwize_Sample_Integration_iOSApp.swift
+//  Mapwize Sample Integration iOS
+//
+//  Created by Robert Tanase on 29/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct Mapwize_Sample_Integration_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
